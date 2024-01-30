b3ca828d7f7413204a663ffc5ef11f1a0c18f209
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-01-30 14:45:31 +0100

    Configures Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.astro"],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      black: "#000000",
+      dark: "#222831",
+      grey: "#393E46",
+      teal: "#00ADB5",
+      light: "#EEEEEE",
+    },
     extend: {},
   },
   plugins: [],
