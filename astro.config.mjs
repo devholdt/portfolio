d22261dcc56540bd34ff16f4e0f8d712c35b2659
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-06-04 15:42:40 +0200

    adds cloudflare to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: cloudflare()
 });
