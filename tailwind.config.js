b941a81d4705ffd8c40ffdb5b86ca94cd084502f
MHerholdt94 <mathiasbherholdt@gmail.com>
2024-06-06 15:33:38 +0200

    changes styling of project cards and adds a layout toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,6 +74,40 @@ module.exports = {
             "background-color": "SelectedItem",
           },
         },
+        ".before-list::before": {
+          content: '""',
+          "z-index": "-1",
+          position: "absolute",
+          inset: "0",
+          "border-radius": "999rem",
+          "transition-property": "transform, color",
+          "transition-duration": "200ms",
+          "transition-timing-function": "ease-in-out",
+        },
+        ".grid .before-list::before": {
+          transform: "translateX(100%)",
+        },
+        '.grid .icon-grid, .not-grid .icon-list, button[aria-pressed="false"] .icon-list':
+          {
+            color: "#EEEEEE",
+          },
+        "@media (prefers-reduced-motion: no-preference)": {
+          ".icon, .before-list::before": {
+            transition: "transform 0.2s ease-in-out, color 0.2s ease-in-out",
+          },
+        },
+      });
+    },
+    function ({ addVariant, e }) {
+      addVariant("grid", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.grid .${e(`grid${separator}${className}`)}`;
+        });
+      });
+      addVariant("list", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.list .${e(`list${separator}${className}`)}`;
+        });
       });
     },
   ],
